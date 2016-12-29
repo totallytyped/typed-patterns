f32045fa56ffa3f6be0febfb1753287102f14f68
Danila <fourlessthanthree@yahoo.com>
2016-12-29 21:38:20 +0300

    Minor jest-preprocessor fix

--- jest-preprocessor.js
+++ jest-preprocessor.js
@@ -1,18 +1,11 @@
 const tsc = require('typescript');
+const tsconfig = require('./tsconfig');
 
 module.exports = {
-  process(src, path) {
-    if (path.endsWith('.ts') || path.endsWith('.tsx')) {
-      return tsc.transpile(
-        src,
-        {
-          module: tsc.ModuleKind.CommonJS,
-          jsx: tsc.JsxEmit.React,
-        },
-        path,
-        []
-      );
+    process(src, path) {
+        if (path.endsWith('.ts') || path.endsWith('.tsx')) {
+            return tsc.transpileModule(src, tsconfig.compilerOptions, path, []);
+        }
+        return src;
     }
-    return src;
-  },
 };
